048f440f9b7ae36378ece98c6c9d1d1b701519aa
Daniel F. Luker <daniel.f.luker@gmail.com>
2016-01-28 18:50:49 -0500

    Adding changes to insert new sections into the page. Mobile is *not* working

--- production-page/scripts/cufon-fonts.js
+++ production-page/scripts/cufon-fonts.js
@@ -4,4 +4,6 @@ Cufon.replace('.intro-message h3', {fontFamily: 'Museo Slab 100'});
 Cufon.replace('.intro-message h3 span', {fontFamily: 'Abraham Lincoln'});
 Cufon.replace('.octagon1 h3', {fontFamily: 'Museo Slab 100'});
 Cufon.replace('.octagon1 h3 strong', {fontFamily: 'Museo Slab 700'});
+Cufon.replace('.intro p', {fontFamily: 'Museo Slab 100'})
+Cufon.replace('.intro p strong', {fontFamily: 'Museo Slab 700'});
 Cufon.replace('.network-name', {fontFamily: 'Museo Slab 100'});
