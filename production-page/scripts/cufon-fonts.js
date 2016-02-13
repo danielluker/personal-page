ba1473be5de1ef53f33a9fcbda15f33418dc0257
Daniel F. Luker <daniel.f.luker@gmail.com>
2016-02-13 16:20:23 -0500

    Added details to the education section, and changed height to min-height in the height definitions for each section

--- production-page/scripts/cufon-fonts.js
+++ production-page/scripts/cufon-fonts.js
@@ -4,10 +4,15 @@ Cufon.replace('.intro-message h2', {fontFamily: 'Museo Slab 100'});
 Cufon.replace('.intro-message h3 span', {fontFamily: 'Abraham Lincoln'});
 Cufon.replace('.octagon1 h3', {fontFamily: 'Museo Slab 100'});
 Cufon.replace('.octagon1 h3 strong', {fontFamily: 'Museo Slab 700'});
-Cufon.replace('p', {fontFamily: 'Museo Slab 100'})
-Cufon.replace('p strong', {fontFamily: 'Museo Slab 700'});
 Cufon.replace('.network-name', {fontFamily: 'Museo Slab 100'});
+
+Cufon.replace('p', {fontFamily: 'Museo Slab 100'})
 Cufon.replace('h1', {fontFamily: 'Museo Slab 700'});
 Cufon.replace('h2', {fontFamily: 'Museo Slab 100'});
 Cufon.replace('h3', {fontFamily: 'Museo Slab 100'});
 Cufon.replace('h4', {fontFamily: 'Museo Slab 100'});
+Cufon.replace('p strong', {fontFamily: 'Museo Slab 700'});
+Cufon.replace('h1 strong', {fontFamily: 'Museo Slab 700'});
+Cufon.replace('h2 strong', {fontFamily: 'Museo Slab 700'});
+Cufon.replace('h3 strong', {fontFamily: 'Museo Slab 700'});
+Cufon.replace('h4 strong', {fontFamily: 'Museo Slab 700'});
