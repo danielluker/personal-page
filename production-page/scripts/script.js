145e7b5822afc7a683c87444cae534eaa9f52f5c
Daniel F. Luker <daniel.f.luker@gmail.com>
2016-02-13 17:55:52 -0500

    Removing debug message and improving text display on mobile

--- production-page/scripts/script.js
+++ production-page/scripts/script.js
@@ -2,10 +2,8 @@
 // Want to show the names on the icons if it's a phone (there's no hover)
 if (/Android|webOS|iPhone|iPad|iPod|BlackBerry|BB|PlayBook|IEMobile|Windows Phone|Kindle|Silk|Opera Mini/i.test(navigator.userAgent)) {
 	$(".octagon h3").css('display', 'block');
-	$(".octagon img").css('-webkit-filter', 'brightness(50%)');
-	$(".octagon img").css('-moz-filter', 'brightness(50%)');
-	$(".octagon img").css('filter', 'url(#brightness)');
-	$(".octagon img").css('filter', 'brightness(50%)');
+	$(".octagon h3").css('filter','brightness(100%)');
+	$(".octagon img").css('-webkit-filter', 'brightness(40%)');
+	$(".octagon img").css('-moz-filter', 'brightness(40%)');
+	$(".octagon img").css('filter', 'brightness(40%)');
 }
-
-alert(navigator.userAgent);
