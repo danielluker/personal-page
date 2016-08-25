320e4d9d7ceee7f92fffc46574379713d59e0df7
Daniel F. Luker <daniel.f.luker@gmail.com>
2016-08-24 20:10:24 -0400

    Fixed font syling

--- production-page/scripts/cufon-fonts.js
+++ production-page/scripts/cufon-fonts.js
@@ -1,18 +1,25 @@
-// Cufon.replace('.intro-message h1', {fontFamily: 'Inspiration'});
-Cufon.replace('.intro-message h1', {fontFamily: 'Museo Slab 700'});
-Cufon.replace('.intro-message h2', {fontFamily: 'Museo Slab 100'});
-Cufon.replace('.intro-message h3 span', {fontFamily: 'Abraham Lincoln'});
+console.log(Cufon.replace('.intro-message h1', {fontFamily: 'Inspiration'}));
+console.log(Cufon.replace('.intro-message h2', {fontFamily: 'Museo Slab 100'}));
+// Cufon.replace('.intro-message h3 span', {fontFamily: 'Abraham Lincoln'});
 Cufon.replace('.octagon1 h3', {fontFamily: 'Museo Slab 100'});
 Cufon.replace('.octagon1 h3 strong', {fontFamily: 'Museo Slab 700'});
+Cufon.replace('.circle h3', {fontFamily: 'Museo Slab 100'});
+Cufon.replace('.circle h3 strong', {fontFamily: 'Museo Slab 700'});
 Cufon.replace('.network-name', {fontFamily: 'Museo Slab 100'});
 
-Cufon.replace('p', {fontFamily: 'Museo Slab 100'})
-Cufon.replace('h1', {fontFamily: 'Museo Slab 700'});
-Cufon.replace('h2', {fontFamily: 'Museo Slab 100'});
-Cufon.replace('h3', {fontFamily: 'Museo Slab 100'});
-Cufon.replace('h4', {fontFamily: 'Museo Slab 100'});
-Cufon.replace('p strong', {fontFamily: 'Museo Slab 700'});
-Cufon.replace('h1 strong', {fontFamily: 'Museo Slab 700'});
-Cufon.replace('h2 strong', {fontFamily: 'Museo Slab 700'});
-Cufon.replace('h3 strong', {fontFamily: 'Museo Slab 700'});
-Cufon.replace('h4 strong', {fontFamily: 'Museo Slab 700'});
+Cufon.replace('.first-section .intro p', {fontFamily: 'Museo Slab 100'})
+Cufon.replace('.first-section .intro p strong', {fontFamily: 'Museo Slab 700'});
+
+Cufon.replace('.section p', {fontFamily: 'Museo Slab 100'});
+Cufon.replace('.section h1', {fontFamily: 'Museo Slab 700'});
+Cufon.replace('.section h2', {fontFamily: 'Museo Slab 100'});
+Cufon.replace('.section h3', {fontFamily: 'Museo Slab 100'});
+Cufon.replace('.section h4', {fontFamily: 'Museo Slab 100'});
+Cufon.replace('.section p strong', {fontFamily: 'Museo Slab 700'});
+Cufon.replace('.section h1 strong', {fontFamily: 'Museo Slab 700'});
+Cufon.replace('.section h2 strong', {fontFamily: 'Museo Slab 700'});
+Cufon.replace('.section h3 strong', {fontFamily: 'Museo Slab 700'});
+Cufon.replace('.section h4 strong', {fontFamily: 'Museo Slab 700'});
+
+
+console.log("Executed cufon font replacement");
